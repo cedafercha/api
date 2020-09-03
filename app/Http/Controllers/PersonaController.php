86494cb5f3c69f2f01891063ac5505be7b6c99b1
Cesar Fernandez <cesarcharri08@gmail.com>
2020-09-02 19:55:15 -0500

    Feat: Se generan archivos necesarios para la primera Api
    	- Se crea modelo Persona.
    	- Se crea migración para BD.
    	- Se modifica api.php para agregar ruta de Api.
    	- Genero método index encargado de listar todas las personas.

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $personas = Persona::all();
+        return $personas;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $id = null)
+    {
+        if(is_null($id)){
+          $persona = new Persona();
+        }else{
+          $persona = Persona::findOrFail($id);
+        }
+
+        $persona->nombre = $request->nombre;
+        $persona->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        self::store($request, $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Persona.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    // Para especificar nombre de la tabla
+    protected $table = 'personas';
+
+    // Para especificar nombre del campo primary key
+    protected $primary_key = 'id';
+
+    // Para especificar que los campos created_at y updated_at se van a utilizar
+    public $timestamps = true;
+
+    protected $hidden = ['created_at', 'updated_at'];
+}
